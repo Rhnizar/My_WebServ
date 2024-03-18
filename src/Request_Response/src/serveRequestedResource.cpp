6954d07287df1502c649841087efada9014b4310
Rida Rhnizar <rrhnizar@c1r2p9.1337.ma>
2024-03-18 01:36:56 +0000

    Preparing tools to explain how this project works

--- src/Request_Response/src/serveRequestedResource.cpp
+++ src/Request_Response/src/serveRequestedResource.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   serveRequestedResource.cpp                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kchaouki <kchaouki@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: rrhnizar <rrhnizar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 23:25:00 by rrhnizar          #+#    #+#             */
-/*   Updated: 2024/03/11 13:04:01 by kchaouki         ###   ########.fr       */
+/*   Updated: 2024/03/18 00:34:32 by rrhnizar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,11 +60,42 @@ void	Response::handleDirectoryRequest(const std::string& Root_ReqPath, const Loc
 		return ;
 	}
 	ResPath = location.getIndexFilePathByRoot(Root_ReqPath); // this function search to file in case not found will return empty
-	if(ResPath.empty() == 0)
+    if(ResPath.empty() == 0)
 	{
-        Req.logging(location.getAccessLog(), 0, "serving file successfully");
-		Fill_Response("200", "OK", FILE_PATH, location);
-		return;
+        size_t pos = str_utils::r_find(ResPath, '.');
+        string  extension = ResPath.substr(pos + 1); // get extension
+	    string bin = location.getCgi()[extension];
+        if (bin.empty())
+        {
+            Req.logging(location.getAccessLog(), 0, "serving file successfully");
+		    Fill_Response("200", "OK", FILE_PATH, location);
+		    return;
+        }
+        else
+        {
+            size_t _pos = str_utils::r_find(ResPath, '/');
+		    std::map<std::string, std::string> env;
+		    env["SCRIPT_NAME"] = ResPath.substr(_pos + 1); //It will be the name of the file
+		    env["SCRIPT_FILENAME"] = ResPath; //It will be the path of the file
+		    env["PATH_INFO"] = ResPath;
+    
+		    env["CONTENT_TYPE"] = Req.getHttp_Header()["Content-Type"];
+		    env["REQUEST_METHOD"] = Req.getReqLine().getMethod();
+		    env["CONTENT_LENGTH"] = Req.getHttp_Header()["Content-Length"];
+		    env["QUERY_STRING"] = Req.getReqLine().getQuery_Params();
+		    env["SERVER_PROTOCOL"] = Req.getReqLine().getHttpVersion();
+    
+		    env["HTTP_COOKIE"] = Req.getHttp_Header()["Cookie"];
+		    env["SERVER_NAME"] = "WebServer";
+		    env["REDIRECT_STATUS"] = "200";
+    
+		    std::string body = Req.getBody();// it will be empty in GET !!!
+		    CGI cgi_obj(body, env, bin);
+		    std::pair<std::string, int> respont = cgi_obj.fill_env();
+            Req.logging(location.getAccessLog(), 0, "Respond: using the CGI Way");
+		    Check_CGI_Response(respont.first, respont.second, location);
+            return;
+        }
 	}
 	if(location.getAutoIndex() == 1)
 	{
--- src/config/src/CustomException.cpp
+++ src/config/src/CustomException.cpp
@@ -6,7 +6,7 @@
 /*   By: rrhnizar <rrhnizar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 10:06:24 by kchaouki          #+#    #+#             */
-/*   Updated: 2024/02/18 00:25:28 by rrhnizar         ###   ########.fr       */
+/*   Updated: 2024/03/18 00:07:55 by rrhnizar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 
 CustomException::CustomException(const std::string& _message) : message(_message)
 {
-	message = "webserve: " + message;
+	message = "webserv: " + message;
 }
 
 CustomException::CustomException(const std::string& _message, const std::string& _arg) : message(_message)
 {
-	message = "webserve: [" + _arg + "] " + message;
+	message = "webserv: [" + _arg + "] " + message;
 }
 
 const char* CustomException::what() const throw(){return (message.c_str());}
